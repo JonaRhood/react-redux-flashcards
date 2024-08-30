79d5f0d75831ee874ddc21548d76b0cc0d989300
Jonathan Cano <jonathancanofreta@gmail.com>
2024-08-30 08:51:07 +0200

    Connecting the store through the Topics features

--- src/features/topics/Topics.js
+++ src/features/topics/Topics.js
@@ -2,9 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import ROUTES from "../../app/routes";
 // import selector
+import { useSelector, useDispatch } from "react-redux";
+import { selectTopics } from "./topicsSlice";
 
 export default function Topics() {
-  const topics = {}; // replace this with a call to your selector to select all the topics in state
+  const topics = useSelector(selectTopics);
+  const dispatch = useDispatch();
 
   return (
     <section className="center">
