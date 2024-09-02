c33f74954a000e047a8c3fa43dd85877623dfd25
Jonathan Cano <jonathancanofreta@gmail.com>
2024-09-02 20:09:48 +0200

    Added Store to localStorage

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import reducers
+import { loadState, saveState } from "./storeToLocalStorage"; 
 import topicsReducer from '../features/topics/topicsSlice';
 import quizzesReducer from '../features/quizzes/quizzesSlice';
 import cardsReducer from '../features/cards/cardsSlice';
 
-export default configureStore({
+
+const preloadedState = loadState();
+
+
+const store = configureStore({
   reducer: {
     topics: topicsReducer,
     quizzes: quizzesReducer,
     cards: cardsReducer
   },
+  preloadedState 
 });
+
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
+
+export default store;
